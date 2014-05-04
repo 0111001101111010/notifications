1a3eceb144d08e3308478c207085f9632a7c6d6b
stanzheng <dev.stanzheng@gmail.com>
2014-05-03 23:16:02 -0400

    login works -missing creds

--- app.js
+++ app.js
@@ -29,8 +29,28 @@ app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+app.get('/', routes);
 app.use('/users', users);
+
+app.use('/install', authUtils.install);
+app.use('/oauth2callback', function(req, res){
+    // if we're able to grab the token, redirect the user back to the main page
+    authUtils.getToken(req.query.code, function(data) {
+        console.log('failure',data);
+        res.end();
+    }, function(oauth2Client, client) {
+        console.log('success',oauth2Client, client);
+        var timelineUtils = new mirror.Timeline(oauth2Client, client);
+        var menu = menuUtils.buildSimpleDefault().build();
+        var card = cardUtils.reset().id(123).title("test").text("hello").menus(menu).build();
+        console.log('try to insert card',card);
+        timelineUtils.insertCard(card, failure, success);
+        res.end();
+    });
+});
+
+
+
 
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
@@ -63,27 +83,8 @@ app.use(function(err, req, res, next) {
     });
 });
 
-
-app.get('/install', authUtils.install);
-app.get('/oauth2callback', function(req, res){
-    // if we're able to grab the token, redirect the user back to the main page
-    authUtils.getToken(req.query.code, function(data) {
-        console.log('failure',data);
-        res.end();
-    }, function(oauth2Client, client) {
-        console.log('success',oauth2Client, client);
-        var timelineUtils = new mirror.Timeline(oauth2Client, client);
-        var menu = menuUtils.buildSimpleDefault().build();
-        var card = cardUtils.reset().id(123).title("test").text("hello").menus(menu).build();
-        console.log('try to insert card',card);
-        timelineUtils.insertCard(card, failure, success);
-        res.end();
-    });
-});
-
-
-app.set('port', process.env.PORT || 3000);
-app.listen(3000);
-console.log("now listening on port: " + 3000);
+//app.set('port', process.env.PORT || 4000);
+app.listen(4000);
+console.log("now listening on port: " + 4000);
 
 module.exports = app;
