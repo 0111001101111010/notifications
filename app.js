6436905475f12ed6d23af22a9391e861604e0e93
stanzheng <dev.stanzheng@gmail.com>
2014-05-03 22:21:55 -0400

    adding mirror utils

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+/**Adding in Mirror utils **/
+var mirror = require('mirror-utils');
+var authUtils = new mirror.Auth();
+var cardUtils = new mirror.Card();
+var contactUtils = new mirror.Contacts();
+var menuUtils = new mirror.Menu();
+
+
 var app = express();
 
 // view engine setup
@@ -54,6 +62,25 @@ app.use(function(err, req, res, next) {
         error: {}
     });
 });
+
+
+app.get('/install', authUtils.install);
+app.get('/oauth2callback', function(req, res){
+    // if we're able to grab the token, redirect the user back to the main page
+    authUtils.getToken(req.query.code, function(data) {
+        console.log('failure',data);
+        res.end();
+    }, function(oauth2Client, client) {
+        console.log('success',oauth2Client, client);
+        var timelineUtils = new mirror.Timeline(oauth2Client, client);
+        var menu = menuUtils.buildSimpleDefault().build();
+        var card = cardUtils.reset().id(123).title("test").text("hello").menus(menu).build();
+        console.log('try to insert card',card);
+        timelineUtils.insertCard(card, failure, success);
+        res.end();
+    });
+});
+
 
 app.set('port', process.env.PORT || 3000);
 app.listen(3000);
